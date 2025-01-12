761fe82c3bd6faf5e9ee57caf0d7ff1c63d44194
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-01-13 00:05:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,5 +14,6 @@ function isSameType(value1, value2) {
 // do not change the code below.
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
-
+if (!isNaN(value1)) value1 = Number(value1);
+if (!isNaN(value2)) value2 = Number(value2);
 alert(isSameType(value1, value2));
